c4196ef3b219c6ab72bf8f68b7f17f42c53959ff
hieudt-2054 <duong.trung.hieu@sun-asterisk.com>
2020-03-27 22:40:13 +0700

    Fix

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var task = require('./modules/viblo');
 var taskTwo = require('./modules/covid');
 var cron = require('node-cron');
-cron.schedule('36 22 * * 1-5', () => {
-    console.log('running a task every minute');
+cron.schedule('0 8 1 * *', () => {
+    console.log('running a task viblo top');
     task.apiViblo()
 });
 
-cron.schedule('38 22 * * 1-5', () => {
-    console.log('running a task every covid');
+cron.schedule('0 8 * * 1-5', () => {
+    console.log('running a task covid');
     taskTwo.apiCovid()
 });
